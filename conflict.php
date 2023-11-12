00ee2d407658ce5bd5720a141f03464144a7bc2a
yuma juliana <150018196+yumajuliana@users.noreply.github.com>
2023-11-12 18:45:54 +0700

    Add files via upload

---
+++ conflict.php
@@ -0,0 +1,27 @@
+<?php
+
+// buat namespace data\satu 
+namespace data\satu {
+    // dengan class conflict
+    class conflict
+    {
+    }
+
+
+    // class sample
+    class sample
+    {
+    }
+    // class dummy
+    class dummy
+    {
+    }
+};
+
+// buat namespace data\dua
+namespace data\dua {
+    // dengan class conflict
+    class conflict
+    {
+    }
+};
